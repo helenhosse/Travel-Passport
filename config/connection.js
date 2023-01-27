906a4d4e2c051182932aadfafd30db53f3df3d1a
Helen Hosse <helenhosse@gmail.com>
2023-01-26 21:42:44 -0700

    added routes and handlebar pages/ day 2

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+if (process.env.heroku_site_name) {        // need to change this when I figure out the heroku site
+    sequelize = new Sequelize(process.env.heroku_site_name);     // this will change too
+} else {
+    sequelize - new Sequelize (
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.heroku_site_name,     // this will change too, should look like JAWSDB_URL
+            port: 3306,
+            dialect: 'mysql',
+        }
+    );
+}
+
+module.exports = sequelize;
---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require ('express').Router();
+
+const categoriesRoutes = require('./categoriesRoutes');
+
+// need to put the API route here as well - no idea how to do
+
+router.use('/categories', categoriesRoutes);
+
+// router.use('/api/user', userApiRoutes); // localhost: /api/user/login - not totally sure how to do this
+
+module.exports = router;
