e0f936a405b2aa976064f9f83c10adda7c0a0ed6
CrownClown014 <114435344+CrownClown014@users.noreply.github.com>
2023-02-01 20:26:58 -0700

    initial DB and Models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,18 @@
 const User = require('./User');
 const Category = require('./Category');
+const Bookings = require('./Bookings');
 
 // will make table essentially here with the models
+
+
+User.hasMany(Bookings, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE",'
+});
+
+Bookings.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+module.exports = {User, Bookings}; 
